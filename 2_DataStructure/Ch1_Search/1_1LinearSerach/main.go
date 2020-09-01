5bc975f467550aec7aa31e3a84e29f7680cc7adb
howard chui <c911010083@gmail.com>
2020-09-01 11:19:44 +0800

    add LinearSearch code

--- 2_DataStructure/Ch1_Search/1_1LinearSerach/main.go
+++ 2_DataStructure/Ch1_Search/1_1LinearSerach/main.go
@@ -4,10 +4,16 @@ import "fmt"
 
 func main() {
 	array := [10]int{5, 8, 13, 15, 2, 7, 3, 11, 4, 18}
+	isFind := false
 	keyWord := 15
 	for i, v := range array {
 		if v == keyWord {
 			fmt.Printf("index:%d value:%d", i, v)
+			isFind = true
+			break
 		}
+	}
+	if !isFind {
+		fmt.Printf("Not Find!")
 	}
 }
