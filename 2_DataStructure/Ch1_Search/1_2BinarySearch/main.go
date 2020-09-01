ca04405605750131cfdd5abdc753a3f4f9df9bce
howard chui <c911010083@gmail.com>
2020-09-01 11:42:17 +0800

    add BinarySearch  code

--- 2_DataStructure/Ch1_Search/1_2BinarySearch/main.go
+++ 2_DataStructure/Ch1_Search/1_2BinarySearch/main.go
@@ -6,11 +6,28 @@ import (
 )
 
 func main() {
-	array := [10]int{5, 8, 13, 15, 2, 7, 3, 11, 4, 18}
+	array := []int{5, 8, 13, 15, 2, 7, 3, 11, 4, 18}
+	key := 15
 	isFind := false
-	keyWord := 15
 	sort.Ints(array)
-	if !isFind {
-		fmt.Printf("Not Find!")
+	mid, max, min := 0, 0, 0
+	max = len(array)
+
+	for max >= min {
+		mid = (min + max) >> 1
+		if array[mid] == key {
+			fmt.Printf("index:%d value:%d\n", mid, key)
+			isFind = true
+			break
+		} else if array[mid] > key {
+			max = mid - 1
+		} else {
+			min = mid + 1
+		}
 	}
+
+	if !isFind {
+		fmt.Println("Not Find!")
+	}
+
 }
